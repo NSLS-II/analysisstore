746b699d3902f671a9c4a1568fa916fd386a5d45
arkilic <arkilic@bnl.gov>
2016-03-14 14:18:39 -0400

    WIP:AnalysisTailHandler added

--- analysisstore/server/engine.py
+++ analysisstore/server/engine.py
@@ -106,3 +106,56 @@ class AnalysisHeaderHandler(DefaultHandler):
             raise utils._compose_err_msg(500, status='No result for given query')
         else:
             utils.return2client(self, data)
+            
+class AnalysisTailHandler(DefaultHandler):
+    """Handler for analysis_tail insert and query operations.
+    Uses traditional RESTful lingo. get for querying and post for inserts
+
+   Methods
+    -------
+    get()
+        Query analysis_header documents. Query params are jsonified for type preservation
+    post()
+        Insert a analysis_header document.Same validation method as bluesky, secondary
+        safety net.
+    """
+
+    @tornado.web.asynchronous
+    def get(self):
+        database = self.settings['db']
+        query = utils.unpack_params(self)
+        _id = query.pop('_id', None)
+        if _id:
+            raise utils._compose_err_msg(500, reason='No ObjectId based search supported')
+        
+        
+        docs = database.analysis_tail.find(query).sort('time',
+                                                        direction=pymongo.DESCENDING)
+        if not docs:
+            raise utils._compose_err_msg(500, 
+                                        reason='No results found for query',
+                                        data=query)
+        else:
+            utils.return2client(self, docs)
+
+    @tornado.web.asynchronous
+    def post(self):
+        database = self.settings['db']
+        data = ujson.loads(self.request.body.decode("utf-8"))
+        jsonschema.validate(data, utils.schemas['analysis_tail'])
+        try:
+            result = database.analysis_header.insert(data)
+        except perr.PyMongoError:
+            raise utils._compose_err_msg(500,
+                                        status='Unable to insert the document',
+                                        data=data)
+        database.analysis_tail.create_index([('header', pymongo.ASCENDING)],
+                                       unique=True, background=True)
+        database.analysis_tail.create_index([('uid', pymongo.ASCENDING)],
+                                       unique=True, background=True)
+        database.analysis_tail.create_index([('time', pymongo.ASCENDING)],
+                                        unique=False)
+        if not result:
+            raise utils._compose_err_msg(500, status='No result for given query')
+        else:
+            utils.return2client(self, data)
