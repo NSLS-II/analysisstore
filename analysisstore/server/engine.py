2baea021b54987878ffbff0bff7e517bf7d43cd3
arkilic <arkilic@bnl.gov>
2016-03-14 14:33:32 -0400

    WIP:EventHeaderHandler added to the service

--- analysisstore/server/engine.py
+++ analysisstore/server/engine.py
@@ -159,3 +159,54 @@ class AnalysisTailHandler(DefaultHandler):
             raise utils._compose_err_msg(500, status='No result for given query')
         else:
             utils.return2client(self, data)
+    
+    
+class EventHeaderHandler(DefaultHandler):
+    """Handler for event_header insert and query operations.
+    Uses traditional RESTful lingo. get for querying and post for inserts
+    
+    Methods
+    -------
+    get()
+        Query event_header documents. Query params are jsonified for type preservation
+    post()
+        Insert a event_header document.Same validation method as bluesky, secondary
+        safety net.
+    """
+    @tornado.web.asynchronous
+    def get(self):
+        database = self.settings['db']
+        query = utils.unpack_params(self)
+        _id = query.pop('_id', None)
+        if _id:
+            raise utils._compose_err_msg(500, reason='No ObjectId based search supported')        
+        docs = database.event_header.find(query).sort('time',
+                                                      direction=pymongo.DESCENDING)
+        if not docs:
+            raise utils._compose_err_msg(500, 
+                                        reason='No results found for query',
+                                        data=query)
+        else:
+            utils.return2client(self, docs)
+    
+    @tornado.web.asynchronous
+    def post(self):
+        database = self.settings['db']
+        data = ujson.loads(self.request.body.decode("utf-8"))
+        jsonschema.validate(data, utils.schemas['event_header'])
+        try:
+            result = database.analysis_header.insert(data)
+        except perr.PyMongoError:
+            raise utils._compose_err_msg(500,
+                                        status='Unable to insert the document',
+                                        data=data)
+        database.event_header.create_index([('analysis_header', pymongo.ASCENDING)],
+                                       unique=True, background=True)
+        database.event_header.create_index([('uid', pymongo.ASCENDING)],
+                                       unique=True, background=True)
+        database.event_header.create_index([('time', pymongo.ASCENDING)],
+                                        unique=False)
+        if not result:
+            raise utils._compose_err_msg(500, status='No result for given query')
+        else:
+            utils.return2client(self, data)
