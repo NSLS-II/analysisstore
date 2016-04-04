03bbcf6a0682a8a7a72b3de7f77e3155191b952f
arkilic <armanarkilic@gmail.com>
2016-04-04 11:22:14 -0400

    ENH/WIP:More succint conn handling and functional header create

--- analysisstore/client/commands.py
+++ analysisstore/client/commands.py
@@ -3,7 +3,8 @@ import requests
 import ujson
 import jsonschema
 from collections import deque
-
+import uuid
+import time as ttime
 
 class AnalysisClient:
     """Your path to AnalysisStore(yiiiiissssss, camelcase"""
@@ -11,17 +12,52 @@ class AnalysisClient:
         self.host = host
         self.header_list = []
         self.data_ref_list = deque()
-        self._host_url = 'http://{}/'.format(self.host)
-        
+        self._host_url = 
+    
+    @property
+    def host(self):
+        return __host
+    
+    @host.setter
+    def host(self, new_val):
+        self.__host = new_val
+    
+    @property 
+    def _host_url(self):
+        return 'http://{}/'.format(self.host)
+    
+    @property
+    def aheader_url(self):
+        return self._host_url + '/analysis_header'
+    
+    @property
+    def atail_url(self):
+        return self._host_url + '/analysis_tail'    
+    
+    @property
+    def dref_url(self):
+        return self._host_url + '/data_reference'
+    
+    @property
+    def dref_header_url(self):
+        return self._host_url + '/data_reference_header'
+    
     def connection_status(self):
         """Check connection status"""
-        r = requests.get(self._host_url + 'is_connected')
+        r = requests.get(self._host_url + '/is_connected')
+        status = False        
+        if r.status_code == 200:
+            status = True
+        return status
+        
+    def create_analysis_header(self, uid=None, time=None, as_doc=False,
+                               **kwargs):
+        payload = dict(uid=uid if uid else str(uuid.uuid4()), 
+                       time=time if time else ttime.time(), **kwargs)
+        r = requests.get(self.aheader_url, params=ujson.dumps(payload))
         r.raise_for_status()
-        return r.text
-
-    def create_analysis_header(self, **kwargs):
-        pass
-
+        return payload
+        
     def create_analysis_tail(self, **kwargs):
         pass
 
