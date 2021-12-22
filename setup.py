55e5a40bef4a68a8476b1fdd7968dae5abe4afa8
Jun Aishima <jaishima@bnl.gov>
2021-12-22 15:27:35 -0500

    add version info
     * remove python2 support shown in classifier

--- setup.py
+++ setup.py
@@ -10,6 +10,27 @@ except ImportError:
 
 import versioneer
 import os
+import sys
+
+# NOTE: This file must remain Python 2 compatible for the foreseeable future,
+# to ensure that we error out properly for people with outdated setuptools
+# and/or pip.
+min_version = (3, 6)
+if sys.version_info < min_version:
+    error = """
+mxtools does not support Python {0}.{1}.
+Python {2}.{3} and above is required. Check your Python version like so:
+
+python3 --version
+
+This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
+Upgrade pip like so:
+
+pip install --upgrade pip
+""".format(
+        *(sys.version_info[:2] + min_version)
+    )
+    sys.exit(error)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -32,11 +53,11 @@ setup(
     install_requires=requirements,
     license="BSD (3-clause)",
     url="https://github.com/NSLS-II/analysisstore.git",
+    python_requires=">={}".format(".".join(str(n) for n in min_version)),
     packages=find_packages(),
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 3',
     ],
 )
