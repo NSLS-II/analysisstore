67a6c5e8e4c052d37d2ccd5c2e74b6320275836d
Jun Aishima <jaishima@bnl.gov>
2024-01-26 11:06:54 -0500

    add conftest
     * similar structure as used for conftrak tests

---
+++ analysisstore/test/conftest.py
@@ -0,0 +1,49 @@
+import time as ttime
+import uuid
+
+import pytest
+import requests
+import subprocess
+import os
+import sys
+import uuid
+from analysisstore.client.commands import AnalysisClient
+import contextlib
+
+testing_config = dict(
+    host="localhost",
+    port=7601,
+    timezone="US/Eastern",
+    service_port=7601,
+    database="astoretest{0}".format(str(uuid.uuid4())),
+    mongo_uri="mongodb://localhost",
+    mongo_host="localhost",
+    mongo_port=27017,
+)
+
+
+@contextlib.contextmanager
+def astore_startup():
+    ps = subprocess.Popen(
+        [
+            sys.executable,
+            "-c",
+            f"from analysisstore.ignition import start_server; start_server(args={testing_config}, testing=True) ",
+        ],
+    )
+    ttime.sleep(1.3)  # make sure the process is started
+    yield ps
+
+
+@pytest.fixture(scope="session")
+def astore_server():
+    with astore_startup() as astore_fixture:
+        yield
+
+
+@pytest.fixture(scope="function")
+def astore_client():
+    c = AnalysisClient(
+        host=testing_config["mongo_host"], port=testing_config["service_port"]
+    )
+    return c
