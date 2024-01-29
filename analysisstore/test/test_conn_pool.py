3d1c6459e740c310dafaa78d8fe953d65eb4edc6
Jun Aishima <jaishima@bnl.gov>
2024-01-29 11:55:59 -0500

    simplify test

--- analysisstore/test/test_conn_pool.py
+++ analysisstore/test/test_conn_pool.py
@@ -1,30 +1,12 @@
 from ..client.commands import AnalysisClient
 from .testing import TESTING_CONFIG
 import pytest
-from .testing import astore_setup, astore_teardown
 
 
-
-class TestConnPool:
-    def setup_class(self):
-        self.proc = astore_setup()
-
-    def test_client_default(self):
-        pytest.raises(TypeError, AnalysisClient)
-
-
-    def test_client_wconf(self):
-        config = {'host': TESTING_CONFIG['host'],
-                'port': TESTING_CONFIG['port']}
-        conn = AnalysisClient(config)
-
-    def test_client_badconf(self):
-        config = {'host': 'localhost'}
-        pytest.raises(KeyError, AnalysisClient, config)
-        config['port'] = TESTING_CONFIG['port']
-        conn = AnalysisClient(config)
-        conn.host == TESTING_CONFIG['host']
-        conn.port == TESTING_CONFIG['port']
-
-    def teardown_class(self):
-        astore_teardown(self.proc)
+def test_client_badconf():
+    config = {"host": "localhost"}
+    pytest.raises(KeyError, AnalysisClient, config)
+    config["port"] = TESTING_CONFIG["port"]
+    conn = AnalysisClient(config)
+    conn.host == TESTING_CONFIG["host"]
+    conn.port == TESTING_CONFIG["port"]
